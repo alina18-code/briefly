ba5055e6341cac6d0f3339244271e9b4bc4b066a
alina18-code <alinasoomro201118@gmail.com>
2026-07-27 00:25:00 +0500

    create feeds database

---
+++ config.py
@@ -0,0 +1,12 @@
+
+feeds = [
+    {"name": "TechCrunch", "category": "tech", "URL": "https://techcrunch.com/feed/"},
+    {"name": "google news tech section", "category": "tech", "URL": "https://news.google.com/rss/headlines/section/topic/TECHNOLOGY?hl=en-US&gl=US&ceid=US:en"},
+    {"name": "CNBC tech news", "category": "tech", "URL": "https://search.cnbc.com/rs/search/combined/ns/view.xml?partnerId=2000&id=19854910"}
+]
+
+
+
+
+
+
