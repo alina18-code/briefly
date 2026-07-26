ab441fd400b6c76f707397b97e1a3e7026169490
alina18-code <alinasoomro201118@gmail.com>
2026-07-27 00:27:26 +0500

    fixing small mistakes

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
-feeds = [
-    {"name": "TechCrunch", "category": "tech", "URL": "https://techcrunch.com/feed/"},
-    {"name": "google news tech section", "category": "tech", "URL": "https://news.google.com/rss/headlines/section/topic/TECHNOLOGY?hl=en-US&gl=US&ceid=US:en"},
-    {"name": "CNBC tech news", "category": "tech", "URL": "https://search.cnbc.com/rs/search/combined/ns/view.xml?partnerId=2000&id=19854910"}
+FEEDS = [
+    {"name": "TechCrunch", "category": "tech", "url": "https://techcrunch.com/feed/"},
+    {"name": "google news tech section", "category": "tech", "url": "https://news.google.com/rss/headlines/section/topic/TECHNOLOGY?hl=en-US&gl=US&ceid=US:en"},
+    {"name": "CNBC tech news", "category": "tech", "url": "https://search.cnbc.com/rs/search/combined/ns/view.xml?partnerId=2000&id=19854910"}
 ]
 
 
