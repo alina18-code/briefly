6f1f69d8407aae86306f7f8a42a43835b72a1c49
alina18-code <alinasoomro201118@gmail.com>
2026-07-29 00:05:43 +0500

    fix all the bugs

--- scraper.py
+++ scraper.py
@@ -14,27 +14,28 @@ def fetch_articles(feeds):
 
 def get_full_text (url):
    downloaded = trafilatura.fetch_url(url)
-   text = trafilatura.extract(downloaded)
-
-   if text is None or len(text) < 200:      
-    return text
+   text = trafilatura.extract(downloaded)  
+   if text is None or len(text) < 200:   
+      return None 
+   
+   return text
+ 
 
 
 def get_all_full_text (articles):
    all_text = []
    for article in articles:
-      text = get_full_text (article)
-      all_text.append(text)
+      text = get_full_text (article.link)
+      if text is None or len(text) < 200:  
+         all_text.append(text)
 
    return all_text
-      
    
 
 if __name__ == "__main__":
     articles = fetch_articles(FEEDS)
     print(len(articles))
-    #articles_url = get_full_text("https://techcrunch.com/2026/07/27/openais-hugging-face-breach-has-reignited-the-debate-over-alignment-and-control/")
-   # print(articles_url)
+
     articles_text = get_all_full_text(articles)
     print(len(articles_text))
         
