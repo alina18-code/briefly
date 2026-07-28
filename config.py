ffab2313d2737d801d5216de5b5de2802da182ca
alina18-code <alinasoomro201118@gmail.com>
2026-07-29 00:42:30 +0500

    done work on scraper.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 
 FEEDS = [
     {"name": "TechCrunch", "category": "tech", "url": "https://techcrunch.com/feed/"},
-    {"name": "google news tech section", "category": "tech", "url": "https://news.google.com/rss/headlines/section/topic/TECHNOLOGY?hl=en-US&gl=US&ceid=US:en"},
-    {"name": "CNBC tech news", "category": "tech", "url": "https://search.cnbc.com/rs/search/combined/ns/view.xml?partnerId=2000&id=19854910"}
+    {"name": "Engadget", "category": "tech", "url": "https://www.engadget.com/rss.xml"},
 ]
 
 
--- scraper.py
+++ scraper.py
@@ -5,16 +5,21 @@ import trafilatura
 
 def fetch_articles(feeds):
     all_articles = []
+
     for feed in feeds:
         parsed = feedparser.parse(feed["url"])
+
         for entry in parsed.entries:
             all_articles.append(entry)
 
     return all_articles
 
+
+
 def get_full_text (url):
    downloaded = trafilatura.fetch_url(url)
-   text = trafilatura.extract(downloaded)  
+   text = trafilatura.extract(downloaded) 
+
    if text is None or len(text) < 200:   
       return None 
    
@@ -24,13 +29,16 @@ def get_full_text (url):
 
 def get_all_full_text (articles):
    all_text = []
+
    for article in articles:
       text = get_full_text (article.link)
-      if text is None or len(text) < 200:  
-         all_text.append(text)
-
+      if text is None or len(text) < 200: 
+         continue
+      
+      all_text.append(text)
    return all_text
    
+
 
 if __name__ == "__main__":
     articles = fetch_articles(FEEDS)
@@ -38,6 +46,7 @@ if __name__ == "__main__":
 
     articles_text = get_all_full_text(articles)
     print(len(articles_text))
+
         
 
 
