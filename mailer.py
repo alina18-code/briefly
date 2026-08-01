2338b3e876ff6f2a4ca354070588a9dff789b3ff
alina18-code <alinasoomro201118@gmail.com>
2026-08-01 23:40:13 +0500

    work on mailer.py file

---
+++ mailer.py
@@ -0,0 +1,12 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+gmail_address = ("GMAIL_ADDRESS")
+gmail_app_password = ("GMAIL_APP_PASSWORD")
+
+
+
