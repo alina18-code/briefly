0f3cf4b9f82de8de7289ecf40efd5df3397c26a3
alina18-code <alinasoomro201118@gmail.com>
2026-08-01 23:56:25 +0500

    intial commit

--- mailer.py
+++ mailer.py
@@ -9,4 +9,23 @@ gmail_address = ("GMAIL_ADDRESS")
 gmail_app_password = ("GMAIL_APP_PASSWORD")
 
 
+def send_email (subject, body):
+   msg = MIMEMultipart()
+   msg["subject"] = subject
+   msg["From"] = gmail_address
+   msg["To"] = gmail_address
+
+   MIMEText(body, "plain")
+   msg.attach(MIMEText)
+
+   server = smtplib.SMTP("stmp.gmail.com", 587)
+   server.starttls()
+   server.login(gmail_address, gmail_app_password)
+   server.send_message(msg)
+   server.quit()
+
+
+if __name__ == "__main__":
+   send_email("test Email", "This is a test from my python script")
+   
 
